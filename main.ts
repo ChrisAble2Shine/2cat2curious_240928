ab6ca75b4c7599ee6ee90279755dd8b3568f4f8b
ChrisAble2Shine <148521238+ChrisAble2Shine@users.noreply.github.com>
2024-10-06 13:57:14 +0800

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,22 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.vy = -219
+namespace SpriteKind {
+    export const Coin = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    game.gameOver(false)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.gameOver(true)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Coin, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.vy == 0) {
+        mySprite.vy = -150
+    }
+})
+let coin: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundColor(9)
 mySprite = sprites.create(img`
@@ -23,5 +39,27 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 0)
 tiles.setCurrentTilemap(tilemap`level2`)
-mySprite.ay = 370
+mySprite.ay = 300
 scene.cameraFollowSprite(mySprite)
+for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
+    coin = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . f 5 5 5 5 5 5 5 5 5 f . . . 
+        . f 5 5 5 4 4 4 4 4 5 5 5 f . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 4 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 4 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 4 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 4 5 5 5 5 5 5 5 5 f . . 
+        . f 5 5 5 5 4 4 5 5 5 5 5 f . . 
+        . . f 5 5 5 5 5 5 5 5 5 f . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Coin)
+    tiles.placeOnTile(coin, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
